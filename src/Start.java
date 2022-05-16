dd4d1082299fa1bdd4123ebc5dce687413268375
Åsa Algulin <asa.algulin@oru.se>
2022-05-16 17:24:59 +0200

    AlienPerRas fungerar

--- src/Start.java
+++ src/Start.java
@@ -65,7 +65,8 @@ public class Start {
         */
          new LoginPage(idb).setVisible(true);
          new NyregistreraAlien(idb).setVisible(true);
-           
+         new AliensPerRas(idb).setVisible(true);
+        
         
     }
 }
