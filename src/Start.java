b6f17c1e99ded8291b0a7dee6bfe0061f4edd1e4
cAppelina <105006817+cAppelina@users.noreply.github.com>
2022-05-18 11:20:11 +0100

    Ta bort Agent + Ta bort Utrustning

--- src/Start.java
+++ src/Start.java
@@ -65,7 +65,7 @@ public class Start {
         */
          new LoginPage(idb).setVisible(true);
          new NyregistreraAlien(idb).setVisible(true);
-         new AndraOmradesChef(idb).setVisible(true);
+         
            
     }
 }
