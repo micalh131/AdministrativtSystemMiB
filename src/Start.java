838fe6865abf11ba1dbb4efbf577366e8569d220
cAppelina <105006817+cAppelina@users.noreply.github.com>
2022-05-24 13:34:44 +0100

    Aliens per ras design klar

--- src/Start.java
+++ src/Start.java
@@ -67,5 +67,6 @@ public class Start {
          new LoginPage(idb).setVisible(true);
          new SokAlienDatum(idb).setVisible(true);
          new AdminStatus(idb).setVisible(true);
+         new AliensPerRas(idb).setVisible(true);
     }
 }
