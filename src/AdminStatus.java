849e8e1a68e62be0c824e0790ff4ad960623fbe7
cAppelina <105006817+cAppelina@users.noreply.github.com>
2022-05-30 14:34:16 +0200

    adminstatus med combobox

--- src/AdminStatus.java
+++ src/AdminStatus.java
@@ -17,93 +17,78 @@ import oru.inf.InfException;
  * @author miche, aaau, cAppelina
  * 
  * Denna klass möjliggör för en adminagent att lägga 
- * till andra agenter som administrtörer i systemet.
+ * till andra agenter som administratörer i systemet.
  */
 public class AdminStatus extends javax.swing.JFrame {
 
     private InfDB idb;
+    private HjalpDbFunktioner konv;
 
     public AdminStatus(InfDB idb) {
         initComponents();
         this.idb = idb;
-        lblAgentHittad.setVisible(false);
+        fyllValjAgent();
         lblAgentFattAdmin.setVisible(false);
+        konv = new HjalpDbFunktioner(idb);
     }
 
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        btnSok = new javax.swing.JButton();
-        txtNamnSok = new javax.swing.JTextField();
-        lblAgentHittad = new javax.swing.JLabel();
-        cbxAdminRatt = new javax.swing.JCheckBox();
         lblAgentFattAdmin = new javax.swing.JLabel();
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
-
-        btnSok.setText("Sök");
-        btnSok.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btnSokActionPerformed(evt);
-            }
-        });
-
-        txtNamnSok.setColumns(6);
-
-        lblAgentHittad.setText("lblAgentHittad");
-
-        cbxAdminRatt.setText("Ge admin rättighet");
-        cbxAdminRatt.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                cbxAdminRattActionPerformed(evt);
-            }
-        });
+        lblAgentRubrik = new javax.swing.JLabel();
+        lblRubrik = new javax.swing.JLabel();
+        cmbAgenter = new javax.swing.JComboBox<>();
+        btnGeAdminRatt = new javax.swing.JButton();
 
         lblAgentFattAdmin.setFont(new java.awt.Font("Helvetica Neue", 2, 13)); // NOI18N
         lblAgentFattAdmin.setText("lblAgentFattAdminstatus");
 
-        jLabel1.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
-        jLabel1.setText("Skriv in agentens namn");
+        lblAgentRubrik.setFont(new java.awt.Font("Helvetica Neue", 1, 13)); // NOI18N
+        lblAgentRubrik.setText("Välj Agent");
 
-        jLabel2.setFont(new java.awt.Font("Helvetica Neue", 1, 15)); // NOI18N
-        jLabel2.setText("Ge Agent Adminstatus");
+        lblRubrik.setFont(new java.awt.Font("Helvetica Neue", 1, 15)); // NOI18N
+        lblRubrik.setText("Ge Agent Adminstatus");
+
+        cmbAgenter.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Välj" }));
+
+        btnGeAdminRatt.setText("Ge adminbehörighet");
+        btnGeAdminRatt.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnGeAdminRattActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(135, Short.MAX_VALUE)
+                .addComponent(lblRubrik)
+                .addGap(120, 120, 120))
             .addGroup(layout.createSequentialGroup()
                 .addGap(79, 79, 79)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(cbxAdminRatt)
+                    .addComponent(btnGeAdminRatt)
+                    .addComponent(cmbAgenter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(lblAgentFattAdmin)
-                    .addComponent(jLabel1)
-                    .addComponent(btnSok)
-                    .addComponent(txtNamnSok, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(lblAgentHittad))
+                    .addComponent(lblAgentRubrik))
                 .addGap(0, 0, Short.MAX_VALUE))
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(135, Short.MAX_VALUE)
-                .addComponent(jLabel2)
-                .addGap(120, 120, 120))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
                 .addGap(34, 34, 34)
-                .addComponent(jLabel2)
+                .addComponent(lblRubrik)
                 .addGap(18, 18, 18)
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txtNamnSok, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(btnSok)
+                .addComponent(lblAgentRubrik)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(cmbAgenter, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
-                .addComponent(lblAgentHittad)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(cbxAdminRatt)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 36, Short.MAX_VALUE)
+                .addComponent(btnGeAdminRatt)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 80, Short.MAX_VALUE)
                 .addComponent(lblAgentFattAdmin)
                 .addGap(60, 60, 60))
         );
@@ -111,49 +96,44 @@ public class AdminStatus extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btnSokActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnSokActionPerformed
-        String soktNamn = txtNamnSok.getText();
-        if (Validering.textFaltEjTomtRegEx(soktNamn)) {
+    private void btnGeAdminRattActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGeAdminRattActionPerformed
+        String namn = cmbAgenter.getSelectedItem().toString();
+        String agentId = konv.getAgentId(namn);
+        String fraga = "UPDATE Agent SET Administrator = 'J' WHERE Namn =" + "'" + namn + "'" + ";";
+        if (Validering.kollaCboxRegEx(namn)) {
             try {
-
-                String fraga = "SELECT Namn FROM Agent WHERE Agent.namn = '" + soktNamn + "'";
-                String svar = idb.fetchSingle(fraga);
-                lblAgentHittad.setVisible(true);
-                lblAgentHittad.setText(svar);
-
-                if (svar == null) {
-                    System.out.println("Agent hittades inte");
-                    JOptionPane.showMessageDialog(null, "Agent hittades inte, skriv in rätt Agentnamn");
-                }
-
+                idb.update(fraga);
+                lblAgentFattAdmin.setVisible(true);
+                lblAgentFattAdmin.setText(namn + " har fått adminstatus");
             } catch (InfException e) {
-                JOptionPane.showMessageDialog(null, "Något gick fel, det gick inte att hitta användaren i databasen");
+                JOptionPane.showMessageDialog(null, "Något gick fel, det gick inte att lägga till administratörsstatus");
                 System.out.println(e.getMessage());
             }
         }
-    }//GEN-LAST:event_btnSokActionPerformed
 
-    private void cbxAdminRattActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cbxAdminRattActionPerformed
-        String agentNamn = lblAgentHittad.getText();
-        String fraga = "UPDATE Agent SET Administrator = 'J' WHERE Namn =" + "'" + agentNamn + "'" + ";";
+    }//GEN-LAST:event_btnGeAdminRattActionPerformed
+
+    private void fyllValjAgent() {
+        String fraga = "SELECT Namn FROM Agent";
+
+        ArrayList<String> allaAgenter;
+
         try {
-            idb.update(fraga);
-            lblAgentFattAdmin.setVisible(true);
-            lblAgentFattAdmin.setText(agentNamn + " har fått adminstatus");
-        } catch (InfException e) {
-            JOptionPane.showMessageDialog(null, "Något gick fel, det gick inte att lägga till administratörsstatus");
-            System.out.println(e.getMessage());
-        }
-        System.out.println(fraga);
-    }//GEN-LAST:event_cbxAdminRattActionPerformed
+            allaAgenter = idb.fetchColumn(fraga);
 
+            for (String plats : allaAgenter) {
+                cmbAgenter.addItem(plats);
+            }
+        } catch (InfException e) {
+            JOptionPane.showMessageDialog(null, "Gick ej att ladda agenter");
+        }
+    }
+    
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btnSok;
-    private javax.swing.JCheckBox cbxAdminRatt;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
+    private javax.swing.JButton btnGeAdminRatt;
+    private javax.swing.JComboBox<String> cmbAgenter;
     private javax.swing.JLabel lblAgentFattAdmin;
-    private javax.swing.JLabel lblAgentHittad;
-    private javax.swing.JTextField txtNamnSok;
+    private javax.swing.JLabel lblAgentRubrik;
+    private javax.swing.JLabel lblRubrik;
     // End of variables declaration//GEN-END:variables
 }
--- src/Start.java
+++ src/Start.java
@@ -65,7 +65,6 @@ public class Start {
         */
 
          new LoginPage(idb).setVisible(true);
-       
         
     }
 }
